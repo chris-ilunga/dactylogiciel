c741c4e90f0ea6ddec1e1f63fd40ee86a4f616b7
glodikisula21 <21kk223@esisalama.org>
2024-06-16 12:51:56 +0200

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,12 @@
+
+#ifndef UTILS_H
+#define UTILS_H
+
+// Fonction pour afficher les instructions du jeu
+void afficherInstructions();
+
+// Fonction pour configurer les mots du jeu
+void definirMots(char listeMots[][100], int *nombreMots);
+
+#endif
+
